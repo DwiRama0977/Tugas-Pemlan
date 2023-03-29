979d2a55c729603d7ed5e94031065e61796ffdab
DwiRama0977 <dwirama0977@gmail.com>
2023-03-29 16:02:28 +0700

    DONE

---
+++ Tugas3/Hero.java
@@ -0,0 +1,64 @@
+package Tugas3;
+
+public class Hero {
+    private String nama;
+    private double hp;
+    private double atk;
+
+    public Hero (String nama, double atk, double hp){
+        this.nama = nama;
+        this.atk = atk;
+        this.hp = hp;
+    }
+
+    public Hero (){
+    }
+    
+    public void setNama (String nama){
+        this.nama = nama;
+    }
+
+    public void setAtk (double atk){
+        this.atk = atk;
+    }
+
+    public void setHp (double hp){
+        this.hp = hp;
+    }
+
+    public String getNama (){
+        return this.nama;
+    }
+
+    public double getAtk (){
+        return this.atk;
+    }
+
+    public double getHp (){
+        return this.hp;
+    }
+
+    public void tampil (){
+        System.out.println("Nama hero\t\t: " + this.nama);
+        System.out.println("Attack hero\t\t: " + this.atk);
+        System.out.println("Health Poin hero\t: " + this.hp);
+        System.out.println();
+    }
+
+    public void lawan (Hero musuh) {
+        System.out.println(this.nama + " menyerang " + musuh.nama);
+        System.out.println(musuh.nama + " mendapat damage sebesar " + this.atk);
+        
+        musuh.menyerang(this.atk);
+        System.out.println("hp " + musuh.nama + " tersisa " + musuh.hp);
+        System.out.println();
+        if (musuh.hp <= 0) {
+            System.out.println("=============HASIL=============");
+            System.out.println(this.nama + " telah kalah");
+        }
+    }
+    
+    public void menyerang (double atk){
+        this.hp -= atk;
+    }
+}
---
+++ Tugas3/Hero_Main.java
@@ -0,0 +1,55 @@
+package Tugas3;
+
+import java.util.Scanner;
+
+public class Hero_Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("===============================");
+        System.out.println("    GAME PERANG ANTAR HERO");
+        System.out.println("===============================");
+
+        Hero hero1 = new Hero ();
+        System.out.print("Masukkan nama hero 1\t: ");
+        hero1.setNama(input.nextLine());
+        System.out.print("Masukkan attack hero 1\t: ");
+        hero1.setAtk(input.nextDouble());
+        System.out.print("Masukkan hp hero 1\t: ");
+        hero1.setHp(input.nextDouble());
+        System.out.println("==========INFO HERO 1==========");
+        hero1.tampil();
+
+        System.out.print("Masukkan nama hero 2\t: ");
+        String nama = input.next();
+        System.out.print("Masukkan atk hero 2\t: ");
+        double atk = input.nextDouble();
+        System.out.print("Masukkan hp hero 2\t: ");
+        double hp = input.nextDouble();
+        System.out.println("==========INFO HERO 2==========");
+        Hero hero2 = new Hero(nama, atk, hp);
+        hero2.tampil();
+
+        int i = 1;
+        int k = 0;
+        while (hero1.getHp() > 0 && hero2.getHp() > 0) {
+            System.out.println("==========RONDE KE-" + i + "===========");
+            k = i++;
+            hero1.lawan(hero2);
+            if (hero2.getHp() > 0) {
+                hero2.lawan(hero1);
+            }
+        }
+        
+        if (hero1.getHp()<0) {
+            akhir(hero2.getNama());
+        } 
+        else {
+            akhir(hero1.getNama());
+        }
+    }
+    public static void  akhir (String nama) {
+        System.out.println(nama + " memenangkan pertandingan");
+            System.out.println("Pertandingan telah selesai");
+            System.out.println();
+    }
+}
