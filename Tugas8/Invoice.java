792bca1cc8937d16ec9d3b0de3c68a0964a0b6ea
DwiRama0977 <dwirama0977@gmail.com>
2023-05-25 22:15:51 +0700

    DONE

--- Tugas8/Invoice.java
+++ Tugas8/Invoice.java
@@ -4,7 +4,7 @@ public class Invoice implements Payable {
     private String productName;
     private int quantity;
     private int pricePerItem;
-    public Invoice[] invoice2;
+    private Invoice[] invoice2;
 
     public Invoice(String productName, int quantity, int pricePerItem) {
         this.productName = productName;
